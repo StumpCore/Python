6111dfb80af65045b2a40ebcd5bc0fd3a0ce5943
Marc Schinschel <mgs60@bath.ac.uk>
2021-07-27 19:17:43 +0200

    Neuroline Tutorial

--- 02_SpamBot/spam.py
+++ 02_SpamBot/spam.py
@@ -1,54 +1,63 @@
-import smtplib
+import smtplib, ssl
 import threading
+import time
+
 #smtplib , python built in library to send emails SMTP or ESMTP listener daemon
-
-
 class MyThread(threading.Thread):
-    def __init__(self, threadID, name, counter, sdr, rpr):
+    def __init__(self, threadID, name, counter,sdr,pwp,rcv,msg,spm, *args, **kwargs):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.counter = counter
         self.sdr = sdr
-        self.rpr = rpr
+        self.pwp = pwp
+        self.rcv = rcv
+        self.msg = msg
+        self.args = args
+        self.kwargs = kwargs
+        self.spm =spm
 
     def run(self):
         print("Starting " + self.name)
-        sending_mail(self.sdr, self.rpr, self.name)
+        sending_mail(self.sdr, self.pwp, self.rcv, self.msg, self.spm)
         print("Exiting " + self.name)
 
 
-def sending_mail(sadr, radr, *args):
-    port = 1025  # FOr SSL
-    server = smtplib.SMTP("localhost", port=port)
-    sender_adr = sadr
-    receiver_adr = radr
+def sending_mail(sender_adr, pw, receiver_adr, msg, spam, *args, **kwargs):
+    """Sending an email to specific contact.
+    Please use on test purposes only.
+    The file was created for learning purpose only.
+    """
 
-    try:
-        for i in range(1,10000):
-            message = str(args)+ " SPAM No.:"+ str(i)
-            server.sendmail(sender_adr, receiver_adr, message)
+    print(sender_adr, pw, receiver_adr,msg)
 
-    except:
-        print("not possible")
+    #Creating the SMTP_SSL
+    port = 465 #For SSL
+    context = ssl.create_default_context()
 
-#Step: 1 SMTP_SSL() First
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context = context) as server:
+        server.login(sender_adr, pw)
+
+        try:
+            for i in range(1,spam):
+                server.sendmail(sender_adr, receiver_adr, msg)
+    
+        except SMTPException as e:
+            print("Error: unable to send email:", str(e))
 
 
-sendermail = "Sender@gmx.net"
-receivermail = "Receiver@gmx.net"
+if __name__ == '__main__':
+
+    sender_adr = input("Please enter your sender gmail account: ")
+    pw = input("Please enter your sender email-pw: ")
+    receiver_adr = input("Please enter your target email: ")
+    msg = input("Please enter your message: ")
+    spm_ct = input("Please enter your SPAM amount: ")
 
 
-t1 = MyThread(1, "Thread-1",1, sendermail, receivermail)
-t2 = MyThread(2, "Thread-2",2, sendermail, receivermail)
-t3 = MyThread(3, "Thread-3",3, sendermail, receivermail)
-t4 = MyThread(4, "Thread-4",4, sendermail, receivermail)
-t5 = MyThread(5, "Thread-5",5, sendermail, receivermail)
+    #increase number of threads by copy
+    t1 = MyThread(1, "Thread-1",1,sender_adr,pw,receiver_adr,msg, spm_ct)
+    t1.start()
 
-t1.start()
-t2.start()
-t3.start()
-t4.start()
-t5.start()
 
 
